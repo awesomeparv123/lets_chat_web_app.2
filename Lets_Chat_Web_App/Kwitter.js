4a21e7f265cd94b97060c5850b931807f8fb092a
awesomeparv123 <151859802+awesomeparv123@users.noreply.github.com>
2024-08-08 20:33:00 +0400

    Add files via upload

---
+++ Lets_Chat_Web_App/Kwitter.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDG3x_9Xgp64n90BmNOuTcG6hqQaJQ7G94",
+    authDomain: "practice-2029c.firebaseapp.com",
+    databaseURL: "https://practice-2029c-default-rtdb.firebaseio.com",
+    projectId: "practice-2029c",
+    storageBucket: "practice-2029c.appspot.com",
+    messagingSenderId: "1082311513342",
+    appId: "1:1082311513342:web:308b747b66707c108841af"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
